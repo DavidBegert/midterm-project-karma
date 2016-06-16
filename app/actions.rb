470b5911b19db5cb67b7f9c7c455bbc78278b33e
Matt Breden <matt_bred@hotmail.com>
2016-06-15 18:38:03 -0700

    Seperatd routes into routes/ folder

--- app/actions.rb
+++ app/actions.rb
@@ -3,28 +3,9 @@ set sessions: true
 
 helpers MyHelpers
 
-before do 
-  #stuff
-end
-
 get '/' do
   erb :index
 end
 
-get '/users/:id' do
-  erb :user
-end
-
-post '/' do
-
-end
-
-# User is signing in.
-# post '/users/signin' do
-#   if user = User.authenticate(params)
-#     sign_user_in(user)
-#   else
-#     sign_user_out
-#   end
-#   redirect '/'
-# end
+require_relative './routes/deed_routes'
+require_relative './routes/user_routes'
---
+++ app/routes/deed_routes.rb
@@ -0,0 +1,9 @@
+# User is praising/shaming a deed.
+post '/deeds/:id/vote' do
+
+end
+
+# New deed
+post '/deeds/new' do
+
+end
