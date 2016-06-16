7266328a4c172de31f9ea7d27c56c86c6f3c0dd4
Matt Breden <matt_bred@hotmail.com>
2016-06-15 17:42:54 -0700

    Added some deeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 # Create users
-User.create!(username: "MattBreden", password: "mattpass", email: "matt@matt.com", image_url: "http://blogdailyherald.com/wp-content/uploads/2014/10/wallpaper-for-facebook-profile-photo.jpg")
-User.create!(username: "DavidBegert", password: "davidpass", email: "david@david.com", image_url: "http://blogdailyherald.com/wp-content/uploads/2014/10/wallpaper-for-facebook-profile-photo.jpg")
-User.create!(username: "Julianaosoume", password: "julanapass", email: "juliana@juliana.com", image_url: "http://blogdailyherald.com/wp-content/uploads/2014/10/wallpaper-for-facebook-profile-photo.jpg")
+matt = User.create!(username: "MattBreden", password: "mattpass", email: "matt@matt.com", image_url: "http://blogdailyherald.com/wp-content/uploads/2014/10/wallpaper-for-facebook-profile-photo.jpg")
+david = User.create!(username: "DavidBegert", password: "davidpass", email: "david@david.com", image_url: "http://blogdailyherald.com/wp-content/uploads/2014/10/wallpaper-for-facebook-profile-photo.jpg")
+jul = User.create!(username: "Julianaosoume", password: "julanapass", email: "juliana@juliana.com", image_url: "http://blogdailyherald.com/wp-content/uploads/2014/10/wallpaper-for-facebook-profile-photo.jpg")
 
 # Create deeds
-#TODO Implement
+matt_deed_1 = matt.deeds.create!(summary: "I drank a beer without paying")
+david_deed_1 = david.deeds.create!(summary: "I murdered a dog")
+jul_deed_1 = jul.deeds.create!(summary: "I helped some people study")
 
 # Create votes
-#TODO implement
