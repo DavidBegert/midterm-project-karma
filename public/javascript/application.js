ca77d0d7a2032af0eec87ef8867e360d196b687d
Matt Breden <matt_bred@hotmail.com>
2016-06-17 13:31:22 -0700

    Modularizing ajax calls

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   browser_path = window.location.pathname
-  var pagination_ready;
+  var pagination_ready = true;
   var pagination_load_more = true;
 
-  // Only after the user has asked for more posts does pagination work
-  var user_paginate = false;
+  // Only after the user has asked for posts does pagination work
+  var user_pagination_ready = false;
 
   $('input').keypress(function (e) {     //theres gotta be a way to connect this and the one below it
     if (e.which == 13) {
@@ -70,18 +70,28 @@ $(document).ready(function() {
   });
 
   $("#users-profile-show-all").click(function() {
-    var user_id = this.dataset.userId
-    console.log("user id = " + user_id)
+    user_paginate = true;
+    var server_path = "/users/next-deeds"
+    var params = {"id":document.querySelector("#users-profile-show-all").dataset.userId}
+    var container = $("#user-deeds-container")
+    $(this).addClass("hidden")
     $("#user-loader-gif").removeClass("hidden")
-    $(this).css("display", "none")
-    $.get("/users/next-deeds", {"id":user_id}, function(data) {
-      $("#deeds_container").removeClass("hidden")
-      $("#user-loader-gif").addClass("hidden")
-      $("#deeds_container").css("display", "block")
-      $("#deeds_container").append(data);
-      user_paginate = true;
-    });
+    ajaxRequestPaginate(server_path, params, container, 'user_pagination_success', user_pagination_fail)
   });
+
+  function user_pagination_success() {
+    // Upon successful user pagination
+    container.removeClass("hidden")
+    $("#user-loader-gif").addClass("hidden")
+    container.css("display", "block")
+  }
+
+  function user_pagination_fail() {
+    alert("Failed to load comments")
+    $("#user-loader-gif").addClass("hidden")
+    container.css("display", "block")
+  }
+
 
  $("#confession_submit").click(function(){ 
     var text_area = $("#confession_summary").val().length
@@ -97,7 +107,7 @@ $(document).ready(function() {
     }
   });
 
- function userPath() {
+ function isUserPath() {
   if (browser_path.startsWith("/users/")) {
     if (!isNaN(browser_path.split('/').pop())) {
       return true
@@ -106,19 +116,17 @@ $(document).ready(function() {
   return false
  }
 
-  function ajaxLoadActivity() {
-    var server_path = "/deeds/next"
-    var user_id = 0
-    if (userPath()) {
-      server_path = "/users/next-deeds"
-      user_id = document.querySelector("#users-profile-show-all").dataset.userId
-    }
-    $.get(server_path, {"id":user_id}, function(data) {
+  function ajaxRequestPaginate(server_path, params, div_container, success_func, fail_func) {
+    pagination_ready = false
+    $.get(server_path, params, function(data) {
       if (data.length == 1) {
         pagination_load_more = false;
       }
-      $("#deeds_container").append(data);
-      pagination_ready = false;
+      div_container.append(data);
+      pagination_ready = true;
+      window["user_pagination_success"]()
+    }).fail(function() {
+      fail_func
     });
   }
 
@@ -128,14 +136,17 @@ $(document).ready(function() {
         if ( $('ol.astream > .loadCount:last > li').attr('id') == "noMoreActivities" ) {
           return false;
         }
-        if (pagination_ready) {
-          return false;
+        if (pagination_ready && browser_path == '/') {
+          var server_path = "/deeds/next"
+          var container = $("#deeds_container")
+          ajaxRequestPaginate(server_path, {}, container);
         }
-        if (userPath() && !user_paginate) {
-          return false
+        else if (pagination_ready && isUserPath() && user_pagination_ready) {
+          var server_path = "/users/next-deeds"
+          var params = {"id":document.querySelector("#users-profile-show-all").dataset.userId}
+          var container = $("#user-deeds-container")
+          ajaxRequestPaginate(server_path, params, container)
         }
-        pagination_ready = true
-        ajaxLoadActivity();
       }
     }); 
   }
