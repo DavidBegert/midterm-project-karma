c955fa31b87266360cd2912799cdfe5ca0a88225
DavidBegert <dbegert@usc.edu>
2016-06-17 04:59:55 +0000

    fixed small bug with infinite pagination

--- app/routes/deed_routes.rb
+++ app/routes/deed_routes.rb
@@ -13,6 +13,7 @@ post '/deeds' do
     summary: params[:summary]
     )
   if deed.save
+    session[:pagination_spot] += 1
     erb :'../views/deeds/_show', layout: false, locals: {all_details: true, deed: deed}
   else
     redirect '/'
@@ -20,6 +21,8 @@ post '/deeds' do
 end
 
 get '/deeds/next' do 
+  puts session[:pagination_spot]
+  puts "list.last.id is: #{Deed.last.id}"
   unless session[:pagination_spot] == Deed.last.id
     erb :'../views/deeds/_pagination', layout: false, locals: {all_details: true, list: Deed.all.limit(MAX_SHOW).offset(session[:pagination_spot]).order(id: :desc)}
   end
